7b043ac918b41bcc97483da6ae06ffbcc5147ca6
huynguyen <huy.nguyen@wizeline.com>
2020-04-12 23:31:07 +0700

    first commit

---
+++ src/main/java/loginmicroservicejwt/wizeline/WizelineApplication.java
@@ -0,0 +1,13 @@
+package loginmicroservicejwt.wizeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loginmicroservicejwt/wizeline/WizelineApplicationTests.java
@@ -0,0 +1,13 @@
+package loginmicroservicejwt.wizeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
